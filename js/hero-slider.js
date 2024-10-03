ecbb4f785f3ac85abd58f7d3af1c5f06d36e36ff
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-10-03 13:46:43 +0500

    hero

---
+++ js/hero-slider.js
@@ -0,0 +1,15 @@
+const heroSlider = new Swiper('.hero-swiper', {
+        loop: true,
+        speed: 1500,
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: false,
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.btn__slider_next',
+            prevEl: '.btn__slider_prev',
+        },
+    }
+);
+
