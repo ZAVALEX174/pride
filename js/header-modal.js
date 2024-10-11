cc31c145ed550f5fa6d8a121e64ff70e52c7b9b9
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-10-11 12:48:16 +0500

    madal2

--- js/header-modal.js
+++ js/header-modal.js
@@ -1,12 +1,24 @@
 const headerModalOne = document.querySelector(".header-menu-madal");
-const openModalBtn = document.querySelector(".modal-one");
+const headerModalTwo = document.querySelector(".header-menu-madal2");
+const openModalBtnOne = document.querySelector(".modal-one");
+const openModalBtnTwo = document.querySelector(".modal-two");
 const closeModalBtn = document.querySelector(".header-menu-madal__close-btn");
+const closeModalBtnTwo = document.querySelector(".header-menu-madal__close-btn2");
 
-openModalBtn.addEventListener("click", (e) => {
+openModalBtnOne.addEventListener("click", (e) => {
   e.preventDefault();
   headerModalOne.style.display = "block";
 });
 closeModalBtn.addEventListener("click", (e) => {
   e.preventDefault();
   headerModalOne.style.display = "none";
+});
+
+openModalBtnTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  headerModalTwo.style.display = "block";
+});
+closeModalBtnTwo.addEventListener("click", (e) => {
+  e.preventDefault();
+  headerModalTwo.style.display = "none";
 });
