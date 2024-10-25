4420af40420021116d4b512c09614c13d549d020
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-10-25 14:55:33 +0500

    modal

--- js/modals.js
+++ js/modals.js
@@ -1,38 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-  const modal = document.querySelector('.modals');
-  // console.log(modal);
-  const downloadBtn = document.querySelector('.col3row4 .header-menu-madal__body-top-link');
-  // console.log(downloadBtn);
-  const modalCatalog = modal.querySelector('.modals-catalog');
-  // console.log(modalCatalog);
-  const closeDownloadBtn = modal.querySelector('.modals-catalog__btn-download');
-  // console.log(closeDownloadBtn);
-  const modalForm = modal.querySelector('.modals-connection');
-  // console.log(modalForm);
+    const modal = document.querySelector('.modals');
+    // console.log(modal);
+    const downloadBtn = document.querySelector('.col3row4 .header-menu-madal__body-top-link');
+    // console.log(downloadBtn);
+    const modalCatalog = modal.querySelector('.modals-catalog');
+    // console.log(modalCatalog);
+    const closeDownloadBtn = modal.querySelector('.modals-catalog__btn-download');
+    // console.log(closeDownloadBtn);
+    const modalForm = modal.querySelector('.modals-connection');
+    // console.log(modalForm);
+
+    const modalFormZakaz = modal.querySelector('.modals-zakaz');
+    const closesModalFormZakaz = modalFormZakaz.querySelector('.modals-zakaz__btn');
+    // console.log(modalForm);
 
 
-
-
-  downloadBtn.addEventListener('click', () => {
-    modal.classList.add('modals_opened');
-    modalCatalog.classList.remove('none');
-  })
-
-  closeDownloadBtn.addEventListener('click', () => {
-    modal.classList.remove('modals_opened');
-    modalCatalog.classList.add('none');
-  })
-
-  const openFormBtn = document.querySelectorAll('.cart__btn');
-  // console.log(openFormBtn);
-  openFormBtn.forEach(el => {
-    el.addEventListener('click', (e) => {
-      e.preventDefault();
-      modal.classList.add('modals_opened');
-      modalForm.classList.remove('none');
+    downloadBtn.addEventListener('click', () => {
+        modal.classList.add('modals_opened');
+        modalCatalog.classList.remove('none');
     })
-  })
+
+    closeDownloadBtn.addEventListener('click', () => {
+        modal.classList.remove('modals_opened');
+        modalCatalog.classList.add('none');
+    })
+
+    const openFormBtn = document.querySelectorAll('.cart__btn');
+    // console.log(openFormBtn);
+    openFormBtn.forEach(el => {
+        el.addEventListener('click', (e) => {
+            e.preventDefault();
+            modal.classList.add('modals_opened');
+            modalFormZakaz.classList.remove('none');
+        })
+    })
+
+    closesModalFormZakaz.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        modal.classList.remove('modals_opened');
+        modalFormZakaz.classList.add('none');
+    })
 });
 
 
