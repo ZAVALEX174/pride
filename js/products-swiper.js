10b0b5c6c65871356e0ec4cc056591cda5719b83
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-10-28 19:52:07 +0500

    products-cart fixed

--- js/products-swiper.js
+++ js/products-swiper.js
@@ -1,6 +1,11 @@
 var productsSwiper = new Swiper(".products-swiper", {
-  slidesPerView: 2.8,
-  // centeredSlides: true,
-  spaceBetween: 24,
-  grabCursor: true,
-});               
+    slidesPerView: 3.35,
+    // centeredSlides: true,
+    spaceBetween: 17,
+    grabCursor: true,
+    navigation: {
+        nextEl: '.products-swiper_next',
+        prevEl: '.products-swiper_prev',
+    },
+
+});
