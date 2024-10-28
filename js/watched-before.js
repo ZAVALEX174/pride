1c95e0def32067fbd5e3a37de2ff51a118285b3d
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-10-28 20:50:31 +0500

    swiper btns 2

--- js/watched-before.js
+++ js/watched-before.js
@@ -1,7 +1,10 @@
-
 var watchedBeforeSwiper = new Swiper(".watched-before-swiper", {
-  slidesPerView: 2.8,
-  // loop: true,
-  spaceBetween: 24,
-  freeMode: true,
+    slidesPerView: 2.8,
+    // loop: true,
+    spaceBetween: 24,
+    freeMode: true,
+    navigation: {
+        nextEl: '.watched-before-next__btn',
+        prevEl: '.watched-before-prev__btn',
+    },
 });
