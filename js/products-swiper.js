224520ef83f974953b3d1793c202eac35268210c
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-11-06 14:07:00 +0500

    cart pagination

--- js/products-swiper.js
+++ js/products-swiper.js
@@ -21,3 +21,34 @@ var productsSwiperBefore = new Swiper(".watched-before__product-cart-swiper", {
         prevEl: '.watched-before__product-cart-btn_prev',
     },
 });
+
+const productsSlide = document.querySelectorAll(".products-slide");
+
+
+if (productsSlide) {
+    productsSlide.forEach(el => {
+        let currentCart = el;
+        const cartSwitchItem = currentCart.querySelectorAll(".cart-switch__item");
+        const imagePagination = currentCart.querySelector(".image-pagination");
+
+        if(cartSwitchItem.length > 1) {
+            cartSwitchItem.forEach((el, index) => {
+                // добавляем дата-атрибут каждому item
+                el.setAttribute("data-index", index);
+                imagePagination.innerHTML += `<li class="image-pagination__item ${index == 0 ? 'image-pagination__item_active' : ''}" data-index="${index}"></li>`;
+                el.addEventListener("mouseenter", (e) => {
+                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => {el.classList.remove('image-pagination__item_active');});
+                    currentCart.querySelector(`.image-pagination__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('image-pagination__item_active');
+                })
+
+                el.addEventListener("mouseleave", (e) => {
+                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => {el.classList.remove('image-pagination__item_active');});
+                    currentCart.querySelector(`.image-pagination__item[data-index="0"]`).classList.add('image-pagination__item_active');
+                })
+
+            })
+        }
+
+    });
+}
+
