3ea66b7afabbbd1ce360bb9396950ef77550a8b6
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-01 18:06:21 +0500

    watched-before slider fixed

--- js/watched-before.js
+++ js/watched-before.js
@@ -1,24 +1,31 @@
+// для всех слайдеров
 var watchedBeforeSwiper = new Swiper(".watched-before-swiper", {
     // slidesPerView: 2.8,
     slidesPerView: "auto",
-    speed: 500,
-    loop: true,
+    // initialSlide: 2,
+    // centeredSlides: true,
+    speed: 1500,
+    // loop: true,
     spaceBetween: 24,
-    freeMode: true,
+    // freeMode: true,
+    // grabCursor: true,
     navigation: {
         nextEl: '.watched-before-next__btn',
         prevEl: '.watched-before-prev__btn',
     },
 });
 
-
+// работает только управление кнопками, все остальное выше
 var watchedBeforeSwiperCatalog = new Swiper(".watched-before__catalog-swiper", {
     // slidesPerView: 2.8,
     slidesPerView: "auto",
-    speed: 500,
-    loop: true,
+    // initialSlide: 2,
+    // centeredSlides: true,
+    speed: 1500,
+    // loop: true,
+    // grabCursor: true,
     spaceBetween: 17,
-    freeMode: true,
+    // freeMode: true,
     navigation: {
         nextEl: '.watched-before__catalog-cart-btn_next',
         prevEl: '.watched-before__catalog-cart-btn_prev',
