04c80d0a8cacc0a6977fee5b1d7492a061b49e05
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-04 11:03:39 +0500

    section .form-representation pixel

--- js/products-swiper.js
+++ js/products-swiper.js
@@ -2,7 +2,7 @@ var productsSwiper = new Swiper(".products-swiper", {
     slidesPerView: "auto",
     speed: 500,
     // centeredSlides: true,
-    loop: true,
+    // loop: true,
     spaceBetween: 17,
     grabCursor: true,
     navigation: {
@@ -33,18 +33,18 @@ if (productsSlide) {
         const cartSwitchItem = currentCart.querySelectorAll(".cart-switch__item");
         const imagePagination = currentCart.querySelector(".image-pagination");
 
-        if(cartSwitchItem.length > 1) {
+        if (cartSwitchItem.length > 1) {
             cartSwitchItem.forEach((el, index) => {
                 // добавляем дата-атрибут каждому item
                 el.setAttribute("data-index", index);
                 imagePagination.innerHTML += `<li class="image-pagination__item ${index == 0 ? 'image-pagination__item_active' : ''}" data-index="${index}"></li>`;
                 el.addEventListener("mouseenter", (e) => {
-                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => {el.classList.remove('image-pagination__item_active');});
+                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => { el.classList.remove('image-pagination__item_active'); });
                     currentCart.querySelector(`.image-pagination__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('image-pagination__item_active');
                 })
 
                 el.addEventListener("mouseleave", (e) => {
-                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => {el.classList.remove('image-pagination__item_active');});
+                    currentCart.querySelectorAll(".image-pagination__item").forEach((el) => { el.classList.remove('image-pagination__item_active'); });
                     currentCart.querySelector(`.image-pagination__item[data-index="0"]`).classList.add('image-pagination__item_active');
                 })
 
