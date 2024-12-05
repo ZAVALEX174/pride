61538338f2c2bb9bda335ae087b31b02b46fc2d9
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-05 21:42:03 +0500

    catalog-menu.html - create hover-effect for card

---
+++ js/catalog-menu.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+  // для удаления и появления hover эффекта
+
+  const catalogMenuCards = document.querySelectorAll('.catalog-menu-categories__body-item');
+  console.log(catalogMenuCards);
+
+  catalogMenuCards.forEach((el) => el.addEventListener('mouseover', () => {
+    const catalogMenuCards2 = document.querySelectorAll('.catalog-menu-categories__body-item');
+    catalogMenuCards2.forEach((elem) => {
+      elem.classList.remove('catalog-menu-categories__body-item-hover');
+    })
+    el.classList.add('catalog-menu-categories__body-item');
+  }))
+});
+
+
+
