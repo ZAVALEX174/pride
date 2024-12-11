729a00e57264a56cf62c8e0df04fef9782a5ca99
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-11 09:43:20 +0500

    catalog-menu.html fixed , style.css fixed

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,6 +1,7 @@
 const heroSlider = new Swiper('.hero-swiper', {
         loop: true,
         speed: 1500,
+        spaceBetween: 10,
         pagination: {
             el: '.swiper-pagination',
             dynamicBullets: false,
