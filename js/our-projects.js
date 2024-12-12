f55561fbac4bad951f495c8ab89827a6209df020
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-12 21:26:55 +0500

    our-projects pixel

--- js/our-projects.js
+++ js/our-projects.js
@@ -3,9 +3,9 @@ var ourProductsSwiper = new Swiper(".our-projects-swiper", {
   slidesPerView: "auto",
   // centeredSlides: true,
   speed: 500,
-  loop: true,
-  spaceBetween: 24,
-  grabCursor: true,
+  // loop: true,
+  spaceBetween: 25,
+  // grabCursor: true,
   navigation: {
     nextEl: '.our-projects-next__btn',
     prevEl: '.our-projects-prev__btn',
