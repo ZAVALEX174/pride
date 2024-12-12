1edb9133fa31b97ab8817296f8c4af346a26da9f
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2024-12-12 09:53:29 +0500

    categories__body-swiper-container fixed in index.html

--- js/hero-slider.js
+++ js/hero-slider.js
@@ -1,16 +1,27 @@
 const heroSlider = new Swiper('.hero-swiper', {
-        loop: true,
-        speed: 1500,
-        spaceBetween: 10,
-        pagination: {
-            el: '.swiper-pagination',
-            dynamicBullets: false,
-            clickable: true,
-        },
-        navigation: {
-            nextEl: '.btn__slider_next',
-            prevEl: '.btn__slider_prev',
-        },
-    }
+    loop: true,
+    speed: 1500,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: false,
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.btn__slider_next',
+        prevEl: '.btn__slider_prev',
+    },
+}
 );
 
+const categoriesSlider = new Swiper('.categories__body-swiper-content-slider', {
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: "auto",
+    // slidesPerView: 3.5,
+    navigation: {
+        nextEl: '.categories__body-swiper-btn-next',
+        prevEl: '.categories__body-swiper-btn-prev',
+    },
+}
+);
