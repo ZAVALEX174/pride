976dce3b8ab6550e629a64270539ff141c303591
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-04 23:02:17 +0500

    news-page-detail.html all create

--- js/news-detail-page-swiper.js
+++ js/news-detail-page-swiper.js
@@ -11,3 +11,24 @@ var swiper = new Swiper(".news-d-swiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+
+// для всех слайдеров
+var watchedBeforeSwiper = new Swiper(".other-news-swiper", {
+  // slidesPerView: 2.8,
+  slidesPerView: "auto",
+  // initialSlide: 2,
+  // centeredSlides: true,
+  speed: 1500,
+  // loop: true,
+  spaceBetween: 20,
+  // freeMode: true,
+  // grabCursor: true,
+  navigation: {
+    nextEl: '.other-news-swiper__btn-next',
+    prevEl: '.other-news-swiper__btn-prev',
+  },
+});
+
+
+
