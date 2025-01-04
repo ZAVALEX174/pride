f1eeb5019f8046cac3e9fd2a25dfa4360f3098fd
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-04 21:50:17 +0500

    news-page-detail.html swiper create

---
+++ js/news-detail-page-swiper.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".news-d-swiper", {
+  cssMode: true,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
