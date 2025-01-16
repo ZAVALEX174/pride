519df785e1401f158efa03f87ea37bd82f4acf32
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-16 14:27:31 +0500

    work - mobile

--- js/relocation-btns.js
+++ js/relocation-btns.js
@@ -10,3 +10,21 @@ for (let item of heroSlidersLiveCollections) {
             <button class="btn contact-us__btn">Связаться с нами</button>
           </div>`;
 }
+
+// Секция work
+const workBodySwiperContentBox = document.querySelector('.work__body-swiper-content-box');
+console.log(workBodySwiperContentBox);
+workBodySwiperContentBox.innerHTML += `<div id="workBodyItemImg" class="swiper-slide work__body-swiper-content-slider-slide work__body-item_img">
+                  <div class="work__body-item">
+                    <h3 class="work__body-cart-title work__body-cart-title_img">
+                      Подбор установки по характеристикам
+                    </h3>
+                    <a
+                      href="#"
+                      class="btn work__body-cart-btn work__body-cart-btn_hover-block">
+                      Подобрать
+                    </a>
+                  </div>
+                </div>
+<a href="catalog.html" class="work__header-link" id="workHeaderLink">Каталог</a>
+`;
