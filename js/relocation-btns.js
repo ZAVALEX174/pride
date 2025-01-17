2006864ce38610bc3ee1c592435eb0fc124be7fd
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-17 11:42:32 +0500

    categories - mobile

--- js/relocation-btns.js
+++ js/relocation-btns.js
@@ -6,15 +6,18 @@ let heroSlidersLiveCollections = heroSliders.children;
 for (let item of heroSlidersLiveCollections) {
     let btnBox = item.querySelector('.hero-slider__buttons');
     // let btnBoxLiveCollection = btnBox.children;
-    btnBox.innerHTML += `<div class="contact-us header__contact-us" id="headerContactUsBtn">
-            <button class="btn contact-us__btn">Связаться с нами</button>
-          </div>`;
+    // btnBox.innerHTML += `<div class="contact-us header__contact-us" id="headerContactUsBtn">
+    //         <button class="btn contact-us__btn">Связаться с нами</button>
+    //       </div>`;
+    btnBox.insertAdjacentHTML("beforeEnd", `<div class="contact-us header__contact-us" id="headerContactUsBtn">
+             <button class="btn contact-us__btn">Связаться с нами</button>
+           </div>`)
 }
 
 // Секция work
 const workBodySwiperContentBox = document.querySelector('.work__body-swiper-content-box');
-console.log(workBodySwiperContentBox);
-workBodySwiperContentBox.innerHTML += `<div id="workBodyItemImg" class="swiper-slide work__body-swiper-content-slider-slide work__body-item_img">
+// console.log(workBodySwiperContentBox);
+workBodySwiperContentBox.insertAdjacentHTML("afterBegin", `<div id="workBodyItemImg" class="swiper-slide work__body-swiper-content-slider-slide work__body-item_img">
                   <div class="work__body-item">
                     <h3 class="work__body-cart-title work__body-cart-title_img">
                       Подбор установки по характеристикам
@@ -27,12 +30,12 @@ workBodySwiperContentBox.innerHTML += `<div id="workBodyItemImg" class="swiper-s
                   </div>
                 </div>
 <a href="catalog.html" class="work__header-link" id="workHeaderLink">Каталог</a>
-`;
+`);
 
 // Секция our-projects
 const ourProjectsSlideOtziv = document.querySelector('.our-projects-swiper-image-box');
-console.log(ourProjectsSlideOtziv);
-ourProjectsSlideOtziv.innerHTML += `<div id="ourProjectsSlideOtziv" class="swiper-slide our-projects-slide our-projects-slide-otziv">
+// console.log(ourProjectsSlideOtziv);
+ourProjectsSlideOtziv.insertAdjacentHTML("afterBegin", `<div id="ourProjectsSlideOtziv" class="swiper-slide our-projects-slide our-projects-slide-otziv">
                   <div class="our-projects-slide__content">
                     <img
                       src="images/works/01.svg"
@@ -53,4 +56,59 @@ ourProjectsSlideOtziv.innerHTML += `<div id="ourProjectsSlideOtziv" class="swipe
                       </button>
                     </div>
                   </div>
-                </div>`;
+                </div>`);
+
+
+// Секция products
+const productBtnLinkBox = document.querySelector('.product-btn-link-box');
+// product-btn-link
+
+productBtnLinkBox.insertAdjacentHTML("afterBegin", `
+ <a href="catalog-menu.html" class="btn product-btn-link" id="productBtnLink" >Каталог</a>
+`);
+
+// Секция categories
+const categoriesBtnsBox = document.querySelector('.categories__btns-box');
+console.log(categoriesBtnsBox);
+// product-btn-link
+
+categoriesBtnsBox.insertAdjacentHTML("afterBegin", `
+ <a href="#" id="categoriesBtnParams" class="btn categories__btn categories__btn_selected">Подбор по параметрам<svg
+                width="25"
+                height="16"
+                viewBox="0 0 25 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M12.625 6.25C14.0747 6.25 15.25 5.07475 15.25 3.625C15.25 2.17525 14.0747 1 12.625 1C11.1753 1 10 2.17525 10 3.625C10 5.07475 11.1753 6.25 12.625 6.25Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+                <path
+                  d="M1.25 3.625H10"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+                <path
+                  d="M15.25 3.625H24"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+                <path
+                  d="M21.375 15C22.8247 15 24 13.8247 24 12.375C24 10.9253 22.8247 9.75 21.375 9.75C19.9253 9.75 18.75 10.9253 18.75 12.375C18.75 13.8247 19.9253 15 21.375 15Z"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+                <path
+                  d="M18.75 12.375H1.25"
+                  stroke="currentColor"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round" />
+              </svg><span class="param-count">+3</span></a>
+ <a href="catalog.html" id="categoriesBtnCatalog" class="btn categories__link">Каталог</a>
+`);
