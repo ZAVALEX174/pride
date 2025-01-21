5c85ddca2f78f6c2de5772a041f3f40b3896c309
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-21 11:02:32 +0500

    fixed Links Header

--- js/maps-icon.js
+++ js/maps-icon.js
@@ -7,24 +7,29 @@ const closeModalMap = document.querySelector('.close');
 openModalMapBtns.forEach((item) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
+
         // e.stopPropagation();
         // console.log(e.target);
         if (item.classList.contains('msk-i')) {
+            item.classList.toggle('icon__black');
             modalMap.classList.add('msk-mod');
             modalMap.style.display = 'flex';
             return;
         }
         if (item.classList.contains('sev-am-i')) {
+            item.classList.toggle('icon__black');
             modalMap.classList.add('sev-am-mod');
             modalMap.style.display = 'flex';
             return;
         }
         if (item.classList.contains('souts-am-i')) {
+            item.classList.toggle('icon__black');
             modalMap.classList.add('souts-am-mod');
             modalMap.style.display = 'flex';
             return;
         }
         if (item.classList.contains('afr-i')) {
+            item.classList.toggle('icon__black');
             modalMap.classList.add('afr-mod');
             modalMap.style.display = 'flex';
             return;
@@ -34,6 +39,7 @@ openModalMapBtns.forEach((item) => {
 
 closeModalMap.addEventListener("click", (e) => {
     e.preventDefault();
+    openModalMapBtns.forEach(el => {el.classList.remove('icon__black');});
     modalMap.style.display = 'none';
     modalMap.classList.remove('msk-mod');
     modalMap.classList.remove('sev-am-mod');
