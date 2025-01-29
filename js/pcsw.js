4f3b0526f18c6ae62f61d4022b7c4656c2bef8ed
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-01-29 12:40:50 +0500

    product-cart.html- mobile(slider)

--- js/pcsw.js
+++ js/pcsw.js
@@ -1,18 +1,38 @@
 var swiper = new Swiper(".mySwiperProd", {
-  loop: true,
-  spaceBetween: 11,
-  slidesPerView: 8.5,
-  freeMode: true,
-  watchSlidesProgress: true,
+  spaceBetween: 5,
+  slidesPerView: 3.5,
+
+  breakpoints: {
+
+    479: {
+      loop: true,
+      spaceBetween: 11,
+      slidesPerView: 8.5,
+      freeMode: true,
+      watchSlidesProgress: true,
+    }
+
+  },
 });
 var swiper2 = new Swiper(".mySwiperProd2", {
-  loop: true,
+  slidesPerView: 1.2,
   spaceBetween: 10,
+  centeredSlides: true,
   navigation: {
     nextEl: ".mySwiperProd2-btn-next",
     prevEl: ".mySwiperProd2-btn-prev",
   },
   thumbs: {
     swiper: swiper,
+  },
+
+  breakpoints: {
+
+    479: {
+      slidesPerView: 1,
+      loop: true,
+      spaceBetween: 10,
+    }
+
   },
 });
