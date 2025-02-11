7772e622c042f9ca26e4bf2c868c0729ff28a1db
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-02-11 19:53:42 +0500

    fixed sliders new section

--- js/about-us-swiper.js
+++ js/about-us-swiper.js
@@ -19,14 +19,14 @@ var mySwiperSpecialists = new Swiper(".mySwiperSpecialists", {
 });
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.8,
+    slidesPerView: 1.574,
     centeredSlides: true,
-    speed: 300,
+    speed: 5000,
     direction: "vertical",
     spaceBetween: 40,
     loop: true,
     autoplay: {
-        delay: 1000,
+        delay: 1,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
     },
@@ -34,16 +34,71 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 1.8,
-    spaceBetween: 40,
-    speed: 500,
-    direction: "vertical",
+    slidesPerView: 1.32,
+    spaceBetween: 22,
+    speed: 5000,
+    direction: "horizontal",
     loop: true,
     autoplay: {
-        delay: 1000,
+        delay: 1,
         disableOnInteraction: false,
-        reverseDirection: true,
+        reverseDirection: false,
         pauseOnMouseEnter: true,
+    },
+
+    breakpoints: {
+
+        390: {
+            slidesPerView: 1.5,
+            spaceBetween: 22,
+            speed: 5000,
+            direction: "horizontal",
+            loop: true,
+            autoplay: {
+                delay: 1,
+                disableOnInteraction: false,
+                reverseDirection: false,
+                pauseOnMouseEnter: true,
+            },
+        },
+        468: {
+            slidesPerView: 1.8,
+            spaceBetween: 22,
+            speed: 5000,
+            direction: "horizontal",
+            loop: true,
+            autoplay: {
+                delay: 1,
+                disableOnInteraction: false,
+                reverseDirection: false,
+                pauseOnMouseEnter: true,
+            },
+        },
+        680: {
+            slidesPerView: 2.5,
+            spaceBetween: 22,
+            speed: 5000,
+            direction: "horizontal",
+            loop: true,
+            autoplay: {
+                delay: 1,
+                disableOnInteraction: false,
+                reverseDirection: false,
+                pauseOnMouseEnter: true,
+            },
+        },
+        769: {
+            slidesPerView: 1.574,
+            spaceBetween: 40,
+            speed: 5000,
+            direction: "vertical",
+            autoplay: {
+                delay: 1,
+                disableOnInteraction: false,
+                reverseDirection: true,
+                pauseOnMouseEnter: true,
+            },
+        }
     },
 });
 
