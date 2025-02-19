7c540b7bec9e88b69005665f9185e1f645f4f452
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-02-19 12:39:01 +0500

    projects-detal-page.html mobile

--- js/news-detail-page-swiper.js
+++ js/news-detail-page-swiper.js
@@ -1,33 +1,49 @@
 var swiper = new Swiper(".news-d-swiper", {
-  cssMode: true,
-  spaceBetween: 20,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  mousewheel: true,
-  keyboard: true,
+    cssMode: true,
+    slidesPerView: 1.13,
+    spaceBetween: 7,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+
+    breakpoints: {
+        640: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+        }
+    },
 });
 
 
 // для всех слайдеров
 var watchedBeforeSwiper = new Swiper(".other-news-swiper", {
-  // slidesPerView: 2.8,
-  slidesPerView: "auto",
-  // initialSlide: 2,
-  // centeredSlides: true,
-  speed: 1500,
-  // loop: true,
-  spaceBetween: 20,
-  // freeMode: true,
-  // grabCursor: true,
-  navigation: {
-    nextEl: '.other-news-swiper__btn-next',
-    prevEl: '.other-news-swiper__btn-prev',
-  },
+
+    slidesPerView: 1.05,
+    // initialSlide: 2,
+    // centeredSlides: true,
+    // speed: 1500,
+    // loop: true,
+    spaceBetween: 14,
+    // freeMode: true,
+    // grabCursor: true,
+    navigation: {
+        nextEl: '.other-news-swiper__btn-next',
+        prevEl: '.other-news-swiper__btn-prev',
+    },
+
+    breakpoints: {
+        424: {
+            slidesPerView: "auto",
+            spaceBetween: 20,
+            speed: 1500,
+        }
+    },
 });
 
 
