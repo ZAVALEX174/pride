cafe6205b6386ea1208406c88b028b6796521280
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-02-19 08:20:05 +0500

    mobile services.html create double slider

--- js/about-us-swiper.js
+++ js/about-us-swiper.js
@@ -1,106 +1,117 @@
-var mySwiperSpecialists = new Swiper(".mySwiperSpecialists", {
-    slidesPerView: 1.01,
-    spaceBetween: 14,
-    speed: 500,
-    // loop: true,
-    // grabCursor: true,
-    navigation: {
-        nextEl: '.mySwiperSpecialists__next',
-        prevEl: '.mySwiperSpecialists__prev',
-    },
-    breakpoints: {
+const specialists = document.querySelector('.mySwiperSpecialists');
 
-        640: {
-            slidesPerView: "auto",
-            spaceBetween: 20,
-        }
-
-    },
-});
-
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1.574,
-    centeredSlides: true,
-    speed: 5000,
-    direction: "vertical",
-    spaceBetween: 40,
-    loop: true,
-    autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-    },
-
-});
-
-var swiper = new Swiper(".mySwiper2", {
-    slidesPerView: 1.32,
-    spaceBetween: 22,
-    speed: 5000,
-    direction: "horizontal",
-    loop: true,
-    autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
-        reverseDirection: false,
-        pauseOnMouseEnter: true,
-    },
-
-    breakpoints: {
-
-        390: {
-            slidesPerView: 1.5,
-            spaceBetween: 22,
-            speed: 5000,
-            direction: "horizontal",
-            loop: true,
-            autoplay: {
-                delay: 1,
-                disableOnInteraction: false,
-                reverseDirection: false,
-                pauseOnMouseEnter: true,
-            },
+if (specialists) {
+    var mySwiperSpecialists = new Swiper(".mySwiperSpecialists", {
+        slidesPerView: 1.01,
+        spaceBetween: 14,
+        speed: 500,
+        // loop: true,
+        // grabCursor: true,
+        navigation: {
+            nextEl: '.mySwiperSpecialists__next',
+            prevEl: '.mySwiperSpecialists__prev',
         },
-        468: {
-            slidesPerView: 1.8,
-            spaceBetween: 22,
-            speed: 5000,
-            direction: "horizontal",
-            loop: true,
-            autoplay: {
-                delay: 1,
-                disableOnInteraction: false,
-                reverseDirection: false,
-                pauseOnMouseEnter: true,
-            },
+        breakpoints: {
+
+            640: {
+                slidesPerView: "auto",
+                spaceBetween: 20,
+            }
+
         },
-        680: {
-            slidesPerView: 2.5,
-            spaceBetween: 22,
-            speed: 5000,
-            direction: "horizontal",
-            loop: true,
-            autoplay: {
-                delay: 1,
-                disableOnInteraction: false,
-                reverseDirection: false,
-                pauseOnMouseEnter: true,
-            },
+    });
+}
+
+const dubleSwiperOne = document.querySelector('.mySwiper');
+const dubleSwiperTwo = document.querySelector('.mySwiper2');
+
+if (dubleSwiperOne && dubleSwiperTwo) {
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1.574,
+        centeredSlides: true,
+        speed: 5000,
+        direction: "vertical",
+        spaceBetween: 40,
+        loop: true,
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
         },
-        769: {
-            slidesPerView: 1.574,
-            spaceBetween: 40,
-            speed: 5000,
-            direction: "vertical",
-            autoplay: {
-                delay: 1,
-                disableOnInteraction: false,
-                reverseDirection: true,
-                pauseOnMouseEnter: true,
+
+    });
+
+    var swiper = new Swiper(".mySwiper2", {
+        slidesPerView: 1.32,
+        spaceBetween: 22,
+        speed: 5000,
+        direction: "horizontal",
+        loop: true,
+        autoplay: {
+            delay: 1,
+            disableOnInteraction: false,
+            reverseDirection: false,
+            pauseOnMouseEnter: true,
+        },
+
+        breakpoints: {
+
+            390: {
+                slidesPerView: 1.5,
+                spaceBetween: 22,
+                speed: 5000,
+                direction: "horizontal",
+                loop: true,
+                autoplay: {
+                    delay: 1,
+                    disableOnInteraction: false,
+                    reverseDirection: false,
+                    pauseOnMouseEnter: true,
+                },
             },
-        }
-    },
-});
+            468: {
+                slidesPerView: 1.8,
+                spaceBetween: 22,
+                speed: 5000,
+                direction: "horizontal",
+                loop: true,
+                autoplay: {
+                    delay: 1,
+                    disableOnInteraction: false,
+                    reverseDirection: false,
+                    pauseOnMouseEnter: true,
+                },
+            },
+            680: {
+                slidesPerView: 2.5,
+                spaceBetween: 22,
+                speed: 5000,
+                direction: "horizontal",
+                loop: true,
+                autoplay: {
+                    delay: 1,
+                    disableOnInteraction: false,
+                    reverseDirection: false,
+                    pauseOnMouseEnter: true,
+                },
+            },
+            769: {
+                slidesPerView: 1.574,
+                spaceBetween: 40,
+                speed: 5000,
+                direction: "vertical",
+                autoplay: {
+                    delay: 1,
+                    disableOnInteraction: false,
+                    reverseDirection: true,
+                    pauseOnMouseEnter: true,
+                },
+            }
+        },
+    });
+
+}
 
 // document.addEventListener("DOMContentLoaded", function () {
 //     const sliderOne = document.querySelector('.slider-one');
