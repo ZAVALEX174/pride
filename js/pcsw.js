2c7609f7cb242ed1da5600604753ed2ce1d27342
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-03-06 20:26:46 +0500

    delete fancybox, create gallery

--- js/pcsw.js
+++ js/pcsw.js
@@ -1,44 +1,46 @@
 var swiper = new Swiper(".mySwiperProd", {
-  spaceBetween: 5,
-  slidesPerView: 3.5,
+    spaceBetween: 5,
+    slidesPerView: 3.5,
 
-  breakpoints: {
+    breakpoints: {
 
-    479: {
-      loop: true,
-      spaceBetween: 11,
-      slidesPerView: 5.5,
-      freeMode: true,
-      watchSlidesProgress: true,
+        479: {
+            loop: true,
+            spaceBetween: 11,
+            slidesPerView: 5.5,
+            freeMode: true,
+            watchSlidesProgress: true,
+        },
+        767: {
+            loop: true,
+            spaceBetween: 11,
+            slidesPerView: 8.5,
+            freeMode: true,
+            watchSlidesProgress: true,
+        },
     },
-    767: {
-      loop: true,
-      spaceBetween: 11,
-      slidesPerView: 8.5,
-      freeMode: true,
-      watchSlidesProgress: true,
-    },
-  },
 });
 var swiper2 = new Swiper(".mySwiperProd2", {
-  slidesPerView: 1.12,
-  spaceBetween: 10,
-  centeredSlides: true,
-  navigation: {
-    nextEl: ".mySwiperProd2-btn-next",
-    prevEl: ".mySwiperProd2-btn-prev",
-  },
-  thumbs: {
-    swiper: swiper,
-  },
+    slidesPerView: 1.12,
+    spaceBetween: 10,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".mySwiperProd2-btn-next",
+        prevEl: ".mySwiperProd2-btn-prev",
+    },
+    thumbs: {
+        swiper: swiper,
+    },
 
-  breakpoints: {
+    breakpoints: {
 
-    479: {
-      slidesPerView: 1,
-      loop: true,
-      spaceBetween: 10,
-    }
+        479: {
+            slidesPerView: 1,
+            loop: true,
+            spaceBetween: 10,
+        }
 
-  },
+    },
 });
+
+
