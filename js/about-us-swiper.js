538c77b32abc3455b56f180e4fa1b87c98750cb7
ЗАВАЛЕКС174 <zuevalex14021983@gmail.com>
2025-03-09 16:39:47 +0500

    filter fixed 1

--- js/about-us-swiper.js
+++ js/about-us-swiper.js
@@ -5,8 +5,6 @@ if (specialists) {
         slidesPerView: 1.01,
         spaceBetween: 14,
         speed: 500,
-        // loop: true,
-        // grabCursor: true,
         navigation: {
             nextEl: '.mySwiperSpecialists__next',
             prevEl: '.mySwiperSpecialists__prev',
@@ -17,7 +15,6 @@ if (specialists) {
                 slidesPerView: "auto",
                 spaceBetween: 20,
             }
-
         },
     });
 }
@@ -112,70 +109,3 @@ if (dubleSwiperOne && dubleSwiperTwo) {
     });
 
 }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//     const sliderOne = document.querySelector('.slider-one');
-//     const sliderTwo = document.querySelector('.slider-two');
-//     let isPausedOne = false;
-//     let isPausedTwo = false;
-//     let interval;
-//
-//     console.log(sliderOne, sliderTwo);
-//
-//     function startSliderOne() {
-//         interval = setInterval(() => {
-//             if (!isPausedOne) {
-//                 const firstSlide = sliderOne.firstElementChild;
-//                 sliderOne.style.transition = 'transform 0.5s ease-in-out';
-//                 sliderOne.style.transform = `translateY(-${firstSlide.offsetHeight}px)`;
-//
-//                 setTimeout(() => {
-//                     sliderOne.style.transition = 'none';
-//                     sliderOne.appendChild(firstSlide);
-//                     sliderOne.style.transform = 'translateY(0)';
-//                 }, 500);
-//             }
-//         }, 2000); // Интервал между слайдами (2 секунды)
-//     }
-//
-// // Остановка слайдера при наведении
-//     sliderOne.addEventListener('mouseenter', () => {
-//         isPaused = true;
-//     });
-//
-// // Возобновление слайдера при уходе курсора
-//     sliderOne.addEventListener('mouseleave', () => {
-//         isPaused = false;
-//     });
-//
-//
-//     function startSliderTwo() {
-//         interval = setInterval(() => {
-//             if (!isPausedTwo) {
-//                 const firstSlide = sliderTwo.firstElementChild;
-//                 sliderTwo.style.transition = 'transform 0.5s ease-in-out';
-//                 sliderTwo.style.transform = `translateY(${firstSlide.offsetHeight}px)`;
-//
-//                 setTimeout(() => {
-//                     sliderTwo.style.transition = 'none';
-//                     sliderTwo.appendChild(firstSlide);
-//                     sliderTwo.style.transform = 'translateY(0)';
-//                 }, 500);
-//             }
-//         }, 2000); // Интервал между слайдами (2 секунды)
-//     }
-//
-// // Остановка слайдера при наведении
-//     sliderTwo.addEventListener('mouseenter', () => {
-//         isPaused = true;
-//     });
-//
-// // Возобновление слайдера при уходе курсора
-//     sliderTwo.addEventListener('mouseleave', () => {
-//         isPaused = false;
-//     });
-//
-// // Запуск слайдера
-//     startSliderOne();
-//     // startSliderTwo();
-// });
